dd37d7a49572975ef1ed84e9b9eaddbe1868fdbc
Tyler Clements <tyler.g.clements@gmail.com>
2020-05-29 10:48:55 -0500

    Completed Exercise 1.

---
+++ dom/shopping-list.js
@@ -0,0 +1,35 @@
+let myUL = document.getElementsByTagName("UL");
+let myInput = document.getElementById("item");
+let myButton = document.getElementsByTagName("Button");
+
+myInput.focus();
+
+myButton[0].addEventListener("click", buttonAdd);
+
+function buttonAdd() {
+
+    let inputVal = myInput.value;
+    myInput.value = "";
+    console.log(inputVal); // remove this later
+
+    let newLI = document.createElement("li");
+    let newSPAN = document.createElement("span");
+    let newBUTTON = document.createElement("Button");
+
+    newLI.append(newSPAN);
+    newLI.append(newBUTTON);
+
+    newSPAN.innerHTML = inputVal;
+    newBUTTON.innerHTML = "Delete"
+
+    myUL[0].append(newLI);
+
+    newBUTTON.addEventListener("click", buttonDelete);
+
+    myInput.focus();
+}
+
+function buttonDelete() {
+
+    this.parentNode.remove();
+}
